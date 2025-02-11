b612d0bbe81c2dd86e41588d0b0f608acb65b9f2
Kalpita27 <kalpitask27@gmail.com>
2025-02-11 16:12:13 +0530

    Fixed Vercel JSON Server deployment

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const jsonServer = require("json-server");
-const path = require("path"); // Import path module
 const server = jsonServer.create();
+const path = require("path");
 const router = jsonServer.router(path.join(__dirname, "db.json")); // Ensure absolute path
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router); // Serve JSON from root
+server.use(router);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
