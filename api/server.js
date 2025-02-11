d27401baa56c7fb2ac5ea43c9972f26c2e0f022f
Kalpita27 <kalpitask27@gmail.com>
2025-02-11 16:27:43 +0530

    Fixed JSON Server deployment

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const jsonServer = require("json-server");
+const path = require("path");
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(process.cwd(), "db.json")); // Use process.cwd()
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+export default server; // ✅ Required for Vercel API
