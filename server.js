2f6adb5d9cba2b9f53d277004498a757b2b786ea
Kalpita27 <kalpitask27@gmail.com>
2025-02-11 16:17:58 +0530

    Fixed JSON Server deployment

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const jsonServer = require("json-server");
-const server = jsonServer.create();
 const path = require("path");
-const router = jsonServer.router(path.join(__dirname, "db.json")); // Ensure absolute path
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json")); // Absolute path to db.json
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
+server.use(router); // Serve JSON from root
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
