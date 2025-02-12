711c2116ddfcdf201e620d6c931c363569fdc9dd
Kalpita27 <kalpitask27@gmail.com>
2025-02-12 23:56:59 +0530

    Enabled CORS for all origin for JSON Server

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
 const jsonServer = require("json-server");
-const path = require("path");
 const cors = require("cors");
+const path = require("path");
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(process.cwd(), "db.json")); // Use process.cwd()
 const middlewares = jsonServer.defaults();
 
-server.use(cors()); 
+server.use(cors({ origin: "*" }));  // ✅ Allow all origins (Netlify requests)
 server.use(middlewares);
 server.use(router);
 
