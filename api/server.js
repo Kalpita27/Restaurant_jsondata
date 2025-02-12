2ad7b384e853bc0e0b008f3624a862ba7165795a
Kalpita27 <kalpitask27@gmail.com>
2025-02-12 23:36:37 +0530

    Enabled CORS for all origin for JSON Server

--- api/server.js
+++ api/server.js
@@ -1,11 +1,12 @@
 const jsonServer = require("json-server");
 const path = require("path");
+const cors = require("cors");
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(process.cwd(), "db.json")); // Use process.cwd()
 const middlewares = jsonServer.defaults();
 
-server.use(cors()); 
+server.use(cors({ origin: "*" })); 
 server.use(middlewares);
 server.use(router);
 
