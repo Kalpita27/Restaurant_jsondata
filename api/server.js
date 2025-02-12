1d842d739ffbbfa889e1e8f7ca431c404c3833cd
Kalpita27 <kalpitask27@gmail.com>
2025-02-12 21:29:33 +0530

    Enabled CORS for JSON Server

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(process.cwd(), "db.json")); // Use process.cwd()
 const middlewares = jsonServer.defaults();
 
+server.use(cors()); 
 server.use(middlewares);
 server.use(router);
 
